d10e9e75d392c292aa2b7fa3ac300e4ff0c9647e
Kevin Huelsmann <kjh311@gmail.com>
2015-07-28 08:51:43 -0700

    added Jims advice on models

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,5 +1,8 @@
 class Drink < ActiveRecord::Base
-  has_many :drink_prices
-  has_many :stores, through: :drink_prices
-  belongs_to :category
+  has_many :prices
+  has_many :stores, through: :prices
+
+  def price(store_id)
+    Price.find_by(drink: self, store_id: store_id).price
+  end
 end
